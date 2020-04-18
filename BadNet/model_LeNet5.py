141bdf1e4e01fa12e9c3d0af28b30a6de6ae2213
GoldfischeRUA <3170101510@zju.edu.cn>
2020-04-18 21:49:30 +0800

    modify model

--- BadNet/model_LeNet5.py
+++ BadNet/model_LeNet5.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class LeNet5(nn.Module):
     def __init__(self, num_class=10):
         super(LeNet5, self).__init__()
-
+#input branch size is 64
         self.c1 = nn.Sequential(
             nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(6),
@@ -18,18 +18,17 @@ class LeNet5(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        self.c3 = nn.Sequential(
-            nn.Conv2d(16, 120, kernel_size=5),
-            nn.BatchNorm2d(120),
+        self.fc1 = nn.Sequential(
+            nn.Linear(16*5*5, 120),
             nn.ReLU()
         )
 
-        self.fc1 = nn.Sequential(
+        self.fc2 = nn.Sequential(
             nn.Linear(120, 84),
             nn.ReLU()
         )
 
-        self.fc1 = nn.Sequential(
+        self.fc3 = nn.Sequential(
             nn.Linear(84, num_class),
             nn.LogSoftmax()
         )
@@ -37,8 +36,8 @@ class LeNet5(nn.Module):
     def forward(self, x):
         out = self.c1(x)
         out = self.c2(out)
-        out = self.c3(out)
-        out = self.reshape(out.size(0), -1)
+        out = self.view(x.size(0), -1)
         out = self.fc1(out)
         out = self.fc2(out)
+        out = self.fc3(out)
         return out
