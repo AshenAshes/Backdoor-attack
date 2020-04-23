3f475057da0af7187483365477c4583e6c72eaa7
AshenAshes <568976222@qq.com>
2020-04-23 18:41:57 +0800

    train

--- BadNet/models.py
+++ BadNet/models.py
@@ -1,7 +1,6 @@
 from torch import nn
 import torch.nn.functional as F
 
-
 class BadNet(nn.Module):
 
     def __init__(self):
@@ -34,3 +33,216 @@ class BadNet(nn.Module):
         for i in size:
             ret *= i
         return ret
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.c1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),#input size:(1,28,28)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+        self.c2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.out = nn.Linear(32*7*7, 10)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.c2(x)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output
+
+class LeNet5(nn.Module):
+    def __init__(self, num_class=10):
+        super(LeNet5, self).__init__()
+#input branch size is 64
+        self.c1 = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2),#input size:(1,28,28)
+            nn.BatchNorm2d(6),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.c2 = nn.Sequential(
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(16*5*5, 120),
+            nn.ReLU()
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU()
+        )
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(84, num_class),
+            nn.LogSoftmax()
+        )
+
+    def forward(self, x):
+        out = self.c1(x)
+        out = self.c2(out)
+        out = self.view(x.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+class VGG16(nn.Module):
+
+    def __init__(self):
+        super(VGG16, self).__init__()
+
+        # 3 * 224 * 224
+        self.conv1_1 = nn.Conv2d(3, 64, 3)  # 64 * 222 * 222
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=(1, 1))  # 64 * 222* 222
+        self.maxpool1 = nn.MaxPool2d((2, 2), padding=(1, 1))  # pooling 64 * 112 * 112
+
+        self.conv2_1 = nn.Conv2d(64, 128, 3)  # 128 * 110 * 110
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=(1, 1))  # 128 * 110 * 110
+        self.maxpool2 = nn.MaxPool2d((2, 2), padding=(1, 1))  # pooling 128 * 56 * 56
+
+        self.conv3_1 = nn.Conv2d(128, 256, 3)  # 256 * 54 * 54
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=(1, 1))  # 256 * 54 * 54
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=(1, 1))  # 256 * 54 * 54
+        self.maxpool3 = nn.MaxPool2d((2, 2), padding=(1, 1))  # pooling 256 * 28 * 28
+
+        self.conv4_1 = nn.Conv2d(256, 512, 3)  # 512 * 26 * 26
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=(1, 1))  # 512 * 26 * 26
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=(1, 1))  # 512 * 26 * 26
+        self.maxpool4 = nn.MaxPool2d((2, 2), padding=(1, 1))  # pooling 512 * 14 * 14
+
+        self.conv5_1 = nn.Conv2d(512, 512, 3)  # 512 * 12 * 12
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=(1, 1))  # 512 * 12 * 12
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=(1, 1))  # 512 * 12 * 12
+        self.maxpool5 = nn.MaxPool2d((2, 2), padding=(1, 1))  # pooling 512 * 7 * 7
+
+        # view
+
+        self.fc1 = nn.Linear(512 * 7 * 7, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
+        # softmax 1 * 1 * 1000
+
+    def forward(self, x):
+        # x.size(0)即为batch_size
+        in_size = x.size(0)
+
+        out = self.conv1_1(x)  # 222
+        out = F.relu(out)
+        out = self.conv1_2(out)  # 222
+        out = F.relu(out)
+        out = self.maxpool1(out)  # 112
+
+        out = self.conv2_1(out)  # 110
+        out = F.relu(out)
+        out = self.conv2_2(out)  # 110
+        out = F.relu(out)
+        out = self.maxpool2(out)  # 56
+
+        out = self.conv3_1(out)  # 54
+        out = F.relu(out)
+        out = self.conv3_2(out)  # 54
+        out = F.relu(out)
+        out = self.conv3_3(out)  # 54
+        out = F.relu(out)
+        out = self.maxpool3(out)  # 28
+
+        out = self.conv4_1(out)  # 26
+        out = F.relu(out)
+        out = self.conv4_2(out)  # 26
+        out = F.relu(out)
+        out = self.conv4_3(out)  # 26
+        out = F.relu(out)
+        out = self.maxpool4(out)  # 14
+
+        out = self.conv5_1(out)  # 12
+        out = F.relu(out)
+        out = self.conv5_2(out)  # 12
+        out = F.relu(out)
+        out = self.conv5_3(out)  # 12
+        out = F.relu(out)
+        out = self.maxpool5(out)  # 7
+
+        # 展平
+        out = out.view(in_size, -1)
+
+        out = self.fc1(out)
+        out = F.relu(out)
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.fc3(out)
+
+        out = F.log_softmax(out, dim=1)
+
+        return out
